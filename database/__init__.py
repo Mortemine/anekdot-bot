faa7793afb2e614771364382544359d43413c207
Alexey <lexalololo@yandex.ru>
2023-03-03 17:28:29 +0200

    init db

---
+++ database/__init__.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+
+def start() -> scoped_session:
+    engine = create_engine("sqlite://", echo=True)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
